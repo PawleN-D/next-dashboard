8ac2eda62243a21cbdc3315de73ab47b0c1139ed
PawleN-D <sirpawle@gmail.com>
2023-10-31 22:30:59 +0200

    add auth to the app

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
 
 export type State = {
     errors?: {
@@ -11,6 +12,20 @@ export type State = {
       status?: string[];
     };
     message?: string | null;
+}
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', Object.fromEntries(formData));
+    } catch (error) {
+      if ((error as Error).message.includes('CredentialsSignin')) {
+        return 'CredentialSignin';
+      }
+      throw error;
+    }
   }
  
 const InvoiceSchema = z.object({
